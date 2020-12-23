06db0e14a3d5b79c9546c5403a7346689fa0d9f2
Thomas Weber <weber_t@mailbox.org>
2020-12-22 19:15:58 -0600

    Update framerate indicator

--- src/components/tw-framerate-indicator/framerate-indicator.jsx
+++ src/components/tw-framerate-indicator/framerate-indicator.jsx
@@ -5,21 +5,18 @@ import {FormattedMessage} from 'react-intl';
 import styles from './framerate-indicator.css';
 
 const FramerateIndicator = ({framerate}) => (
-    // do not show an indicator at 30 FPS
-    framerate === 30 ? null : (
-        <div className={styles.framerateContainer}>
-            <div className={styles.framerateLabel}>
-                <FormattedMessage
-                    defaultMessage="{framerate} FPS"
-                    description="Label indicating project framerate"
-                    id="tw.framerateIndicator"
-                    values={{
-                        framerate: framerate
-                    }}
-                />
-            </div>
+    <div className={styles.framerateContainer}>
+        <div className={styles.framerateLabel}>
+            <FormattedMessage
+                defaultMessage="{framerate} FPS with interpolation"
+                description="Label indicating project framerate when interpolation is enabled"
+                id="tw.framerateIndicatorWithInterpolation"
+                values={{
+                    framerate: framerate
+                }}
+            />
         </div>
-    )
+    </div>
 );
 
 FramerateIndicator.propTypes = {
